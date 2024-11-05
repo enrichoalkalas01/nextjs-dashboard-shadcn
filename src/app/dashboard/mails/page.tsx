f43095c8e445cdb19e43b2d201e49d1e8fd71a97
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2024-11-06 00:55:42 +0700

    add: changes and have some error need to fix from email page components

--- src/app/dashboard/mails/page.tsx
+++ src/app/dashboard/mails/page.tsx
@@ -1,7 +1,51 @@
-export default function Page() {
+import { cookies } from "next/headers";
+import Image from "next/image";
+
+import { accounts, mails } from "@/components/shadcn/emails/data";
+import { Mail } from "@/components/shadcn/emails/mail";
+
+export default async function Page() {
+    const layout = (await cookies()).get("react-resizable-panels:layout:mail");
+    const collapsed = (await cookies()).get("react-resizable-panels:collapsed");
+
+    const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+    const defaultCollapsed = collapsed
+        ? JSON.parse(collapsed.value)
+        : undefined;
+
     return (
         <>
-            <span>asd</span>
+            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+                    <div className="w-full h-full p-4">
+                        <div className="md:hidden">
+                            <Image
+                                src="/examples/mail-dark.png"
+                                width={1280}
+                                height={727}
+                                alt="Mail"
+                                className="hidden dark:block"
+                            />
+                            <Image
+                                src="/examples/mail-light.png"
+                                width={1280}
+                                height={727}
+                                alt="Mail"
+                                className="block dark:hidden"
+                            />
+                        </div>
+                        <div className="hidden flex-col md:flex">
+                            <Mail
+                                accounts={accounts}
+                                mails={mails}
+                                defaultLayout={defaultLayout}
+                                defaultCollapsed={defaultCollapsed}
+                                navCollapsedSize={4}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -15,11 +15,6 @@ export default function Page() {
     return (
         <>
             <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="aspect-video rounded-xl bg-muted/50"></div>
-                    <div className="aspect-video rounded-xl bg-muted/50" />
-                    <div className="aspect-video rounded-xl bg-muted/50" />
-                </div>
                 <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
                     <div className="w-full h-full p-4">
                         <Tabs defaultValue="overview" className="space-y-4">
